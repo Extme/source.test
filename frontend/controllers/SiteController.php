3ebad9257a4d24ba697ecbcc562d300c115ef264
worker2 <v_a_a@live.ru>
2019-03-27 02:46:26 +0200

    fix

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -2,17 +2,7 @@
 namespace frontend\controllers;
 
 use Yii;
-use yii\base\InvalidParamException;
-use yii\web\BadRequestHttpException;
 use yii\web\Controller;
-use yii\filters\VerbFilter;
-use yii\filters\AccessControl;
-use common\models\LoginForm;
-use frontend\models\TestModel;
-use frontend\models\PasswordResetRequestForm;
-use frontend\models\ResetPasswordForm;
-use frontend\models\SignupForm;
-use frontend\models\ContactForm;
 use linslin\yii2\curl;
 
 /**
@@ -41,9 +31,8 @@ class SiteController extends Controller
     }
 
     /**
-     * Displays homepage.
      *
-     * @return mixed
+     * @return array
      */
 
     public function generateTree(){
